830580a49360bda3a2592e45b7c14a0041cad003
Josh Brown <josh@joossh.com>
2024-01-30 09:26:00 +0000

    ⚗️ Explore note taking concept
    Trying to re-create an experience like iA Writer but in Neovim. Feels
    like it could do something very similar and be a nicer fit in a keyboard
    orientated workflow.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,4 +12,38 @@ return {
   },
 
   "theprimeagen/vim-be-good",
+
+  {
+    "folke/twilight.nvim",
+    config = true,
+  },
+
+  "preservim/vim-pencil",
+
+  {
+    "junegunn/goyo.vim",
+    config = function()
+      local goyo_group = vim.api.nvim_create_augroup("GoyoGroup", { clear = true })
+
+      vim.api.nvim_create_autocmd("User", {
+        group = goyo_group,
+        pattern = "GoyoEnter",
+        callback = function()
+          -- Your logic to disable lualine goes here
+          vim.g.lualine_active = false
+          require("lualine").hide()
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        group = goyo_group,
+        pattern = "GoyoLeave",
+        callback = function()
+          -- Your logic to enable lualine goes here
+          vim.g.lualine_active = true
+          require("lualine").hide({ unhide = true })
+        end,
+      })
+    end,
+  },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,13 +5,32 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
-        "bash", "css", "dockerfile", "embedded_template", "go", "html", "javascript", "json", "lua",
-        "php", "python", "ruby", "rust", "scss", "terraform", "tsx", "typescript", "yaml", "vim", "vimdoc",
+        "bash",
+        "css",
+        "dockerfile",
+        "embedded_template",
+        "go",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "php",
+        "python",
+        "ruby",
+        "rust",
+        "scss",
+        "terraform",
+        "tsx",
+        "typescript",
+        "yaml",
+        "vim",
+        "vimdoc",
       },
       auto_install = false,
 
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
