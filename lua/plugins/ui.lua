96baf602df093c0ea73e616f5e08b57c1faa931c
Josh Brown <josh@joossh.com>
2024-02-10 14:11:42 +0000

    💄 Update default theme flavour

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -47,6 +47,7 @@ return {
     priority = 1000,
     config = function()
       require("catppuccin").setup({
+        flavour = "frappe",
         transparent_background = true,
       })
       vim.cmd.colorscheme("catppuccin")
