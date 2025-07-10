9b2a4f6ebcb4424b436fdf6db7ddb1543df77b01
Josh Brown <josh@joossh.com>
2025-07-10 08:20:31 +0100

    Fix ident issues with Ruby

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -29,8 +29,14 @@ return {
       },
       auto_install = false,
 
-      highlight = { enable = true },
-      indent = { enable = true },
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { "ruby" },
+      },
+      indent = { enable = true, disable = { "ruby" } },
     })
   end,
 }
